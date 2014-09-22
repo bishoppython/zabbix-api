5b7b95dfc80aba6eff6a321e498ae14fff34fcbe
taishin <suzuka46@gmail.com>
2014-09-22 10:52:29 +0900

    update rb

--- admin-jp.rb
+++ admin-jp.rb
@@ -1,10 +1,16 @@
 require 'rubygems'
-require 'zbxapi'
+require 'zabbix/client'
 require 'yaml'
 
 config = YAML.load_file("config.yml")
 
-zabbix = ZabbixAPI.new("http://#{config["host"]}/zabbix", :verify_ssl => false, :http_timeout => 300 )
-zabbix.login(config["username"],config["password"])
+host = config["host"]
+username = config["username"]
+password = config["password"]
+
+zabbix = Zabbix::Client.new("http://#{host}/zabbix/api_jsonrpc.php")
+#client = Zabbix::Client.new('http://localhost/zabbix/api_jsonrpc.php')
+
+zabbix.user.login(user: username, password: password)
 
 zabbix.user.update(:userid => "1", :lang => "ja_JP")
--- import.rb
+++ import.rb
@@ -1,73 +1,88 @@
 require 'rubygems'
-require 'zbxapi'
+require 'zabbix/client'
 require 'yaml'
 
 config = YAML.load_file("config.yml")
 
-zabbix = ZabbixAPI.new("http://#{config["host"]}/zabbix", :verify_ssl => false, :http_timeout => 300 )
-zabbix.login(config["username"],config["password"])
+host = config["host"]
+username = config["username"]
+password = config["password"]
 
+zabbix = Zabbix::Client.new("http://#{host}/zabbix/api_jsonrpc.php")
+zabbix.user.login(user: username, password: password)
+
+
+def import_xml(zbx_instance, template)
+  zabbix = zbx_instance
+  zabbix.configuration.import(
+    :rules => {
+      :applications => {
+        :createMissing => true,
+        :updateExisting => true
+      },
+      :discoveryRules => {
+        :createMissing => true,
+        :updateExisting => true
+      },
+      :graphs  => {
+        :createMissing => true,
+        :updateExisting => true
+      },
+      :groups => {
+        :createMissing => true,
+      },
+      :hosts => {
+        :createMissing => true,
+        :updateExisting => true
+      },
+      :images => {
+        :createMissing => true,
+        :updateExisting => true
+      },
+      :items => {
+        :createMissing => true,
+        :updateExisting => true
+      },
+      :maps => {
+        :createMissing => true,
+        :updateExisting => true
+      },
+      :screens => {
+        :createMissing => true,
+        :updateExisting => true
+      },
+      :templateLinkage => {
+        :createMissing => true,
+      },
+      :templates => {
+        :createMissing => true,
+        :updateExisting => true
+      },
+      :templateScreens => {
+        :createMissing => true,
+        :updateExisting => true
+      },
+      :triggers => {
+        :createMissing => true,
+        :updateExisting => true
+      }
+    }, 
+    :source => template, 
+    :format => 'xml'
+  )
+end
+
+f = File.read("./zabbix-template/KW_Template_Virt_VMware_Hypervisor.xml")
+import_xml(zabbix, f)
+
+f = File.read("./zabbix-template/KW_Template_Virt_VMware_Guest.xml")
+import_xml(zabbix, f)
 
 Dir::foreach('./zabbix-template') do |file|
   if file =~ /\.xml$/
-    f = File.read("./zabbix-template/#{file}")
-
-    zabbix.configuration.import(
-      :rules => {
-        :applications => {
-          :createMissing => true,
-          :updateExisting => true
-        },
-        :discoveryRules => {
-          :createMissing => true,
-          :updateExisting => true
-        },
-        :graphs  => {
-          :createMissing => true,
-          :updateExisting => true
-        },
-        :groups => {
-          :createMissing => true,
-        },
-        :hosts => {
-          :createMissing => true,
-          :updateExisting => true
-        },
-        :images => {
-          :createMissing => true,
-          :updateExisting => true
-        },
-        :items => {
-          :createMissing => true,
-          :updateExisting => true
-        },
-        :maps => {
-          :createMissing => true,
-          :updateExisting => true
-        },
-        :screens => {
-          :createMissing => true,
-          :updateExisting => true
-        },
-        :templateLinkage => {
-          :createMissing => true,
-        },
-        :templates => {
-          :createMissing => true,
-          :updateExisting => true
-        },
-        :templateScreens => {
-          :createMissing => true,
-          :updateExisting => true
-        },
-        :triggers => {
-          :createMissing => true,
-          :updateExisting => true
-        }
-      }, 
-      :source => f, 
-      :format => 'xml'
-    )
-
+     f = File.read("./zabbix-template/#{file}")
+    import_xml(zabbix, f)
   end
 end
+
+ 
